3ef7ce46f2edf052030122fa42d004d6b07ef1fe
septicentric <tsybina.av@gmail.com>
2024-10-09 14:08:08 +0300

    Lab 4

---
+++ Task4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	char c = '!';
+	int i = 2; 
+	float f = 3.14f;
+	double d = 5e-12;
+	printf("%c, %d, %f, %e\n", c, i, f, d);
+	puts("Insert char");
+	scanf("%c", &c);
+	puts("Insert int");
+	scanf("%d", &i);
+	puts("Insert float");
+	scanf("%f", &f);
+	puts("Insert double");
+	scanf("%le", &d);
+	printf("%c, %d, %f, %le\n", c, i, f, d);
+	printf("%d + %f\n", (int)f, f - (int)f);
+	printf("%d, %x\n", (int) c, c);
+	printf("%f\n", 1. / i);
+	*/
+
+	/*
+	int a = 11, b = 3;
+	int x= a / b;
+	float y = a / b;
+	double z = a / b;
+	printf("%d, %f, %e\n", x, y, z);
+
+	printf("%f, %e", (float) a/b, (double) a/b);
+	*/
+
+	/*
+	int num;
+	puts("Insert a three-digit number, pls");
+	scanf("%d", &num);
+	printf("First digit - %d\n", num / 100);
+	//printf("Second digit - %d\n", (num - num % 10) / 10  % 10);
+	printf("Third digit - %d\n", num%10);
+	printf("Sum of digits - %d\n", num / 100 + num % 10 + (num - num % 10) / 10 % 10);
+	printf("Number backwards - %d\n", num - (num / 100 * 100) - (num % 10) + (num % 10 * 100) + (num / 100));
+	*/
+
+	/*
+	int num;
+	puts("Insert a three-digit number, pls");
+	scanf("%d", &num);
+	printf("First digit - %d, last digit - %d, sum of digits - %d, number backwards - %d.\n", num / 100, num % 10, num / 100 + num % 10 + (num - num % 10) / 10 % 10, num - (num / 100 * 100) - (num % 10) + (num % 10 * 100) + (num / 100));
+	*/
+
+
+}
