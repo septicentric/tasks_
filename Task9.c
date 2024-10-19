6631a4aa01c699110bf87ed8f9c2aa6e27b2f412
septicentric <tsybina.av@gmail.com>
2024-10-19 16:48:35 +0300

    Lab 9

---
+++ Task9.c
@@ -0,0 +1,93 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define ROW 9
+#define COL 9
+
+void main() {
+	/*
+	int row, col;
+	printf("\n");
+	for (row = 1; row <= ROW; row++) {
+		for (col = 1; col <= row; col++) {
+			printf("%5d", col * row);
+			printf(" ");
+		}
+		printf("\n");
+	}
+
+	int n;
+	printf("Insert a number, pls: ");
+	scanf("%d", &n);
+	for (row = 1; row <= n; row++) {
+		for (col = 1; col <= row; col++) {
+			printf("%5d ", n);
+		}
+		printf("\n");
+	}
+	*/
+	int m = 1; 
+	while (1) {
+		int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 1, a8, a9 = 0, a10 = 0, a11 = 0, a12 = 0;
+		int a11_1 = 0, a11_2 = 0;
+		int a12_1 = 1, a12_2 = 1;
+		char a;
+		unsigned long int n;
+		printf("Pls, enter the number - ");
+		scanf("%d", &n);
+		printf("%d\n", n);
+		int j = 0;
+		for (unsigned long int k = n; k > 0; k /= 10) j++;
+
+		int p;
+		for (int i = 1; i <= j; i++) { //продвижение разряд за разрядом
+			int t = n / pow(10, (j - i));
+			t %= 10;
+			if (t == 3) a1++;
+			if (t == n % 10 && j != i) a2++;
+			if (t % 2 == 0) a3++;
+			if (t > 5) a4++;
+			if (t % 2 != 0) a5 += t;
+			if (t >= 7) a6 += t;
+			if (t != 0) a7 *= t;
+			if (j >= 2 && i == 2) a8 = t;
+			if (i == j - 1) a8 *= t;
+			
+			if (t % 2 == 0 && (i != j || i != j - 1)) a9 += t;
+			if (i > 1 && t == p) a10++;
+			if (i <= j / 2) a11_1 += t;
+			if (i > j / 2 && j % 2 == 0 || i > j / 2 + 1 && j % 2 == 1) a11_2 += t;
+			if (i % 2 == 0) a12_1 += t;
+			if (i % 2 == 1) a12_2 += t;
+
+			p = t; //прошлое перед t
+		}
+		if (a11_1 == a11_2) a11 = 1;
+		a12 = a12_1 * a12_2;
+
+		printf("Num of '3' - %d\n", a1);
+		printf("Num of last num in digit - %d\n", a2);
+		printf("Num of chot nums - %d\n", a3);
+		printf("Num of nums > 5 - %d\n", a4);
+		printf("Sum of nechot nums - %d\n", a5);
+		printf("Sum of nums > 7 - %d\n", a6);
+		printf("Proizv of all nums without '0' - %d\n", a7);
+		printf("Proizv last and second nums - %d\n", a8);
+		printf("Sum of chot nums without last - %d\n", a9);
+		printf("Num of similar nums near - %d\n", a10);
+		if (a11 == 1) {
+			printf("Sum of nums left and right halfs are similar\n");
+		}
+		else {
+			printf("Sum of left and right halfs aren't similar\n");
+		}
+		printf("Proizv of sum chot and nechot nums - %d\n", a12);
+	
+		printf("Do you want to continue? (Yes - y, No - n)\n");
+		scanf("%c", &a);
+
+		if ((a = getchar()) == 'n') break;
+
+	}
+
+
+}
